78d3b4fe45cdbcf26420d307b81a7c7c66375efe
Eugene Elantsev <eugene_el@mail.ru>
2022-11-26 23:34:01 +0700

    create Polygon fields n methods

--- src/main/java/ru/bcomms/modelElements/Polygon.java
+++ src/main/java/ru/bcomms/modelElements/Polygon.java
@@ -1,5 +1,24 @@
 package ru.bcomms.modelElements;
 
-public class Polygon {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Polygon {
+    private final Set<Point3D> points;
+
+    public Polygon() {
+        this.points = new HashSet<>();
+    }
+
+    public Set<Point3D> getPoints() {
+        return points;
+    }
+
+    public boolean addPoint(Point3D point3D) {
+        return points.add(point3D);
+    }
+
+    public boolean removePoint(Point3D point3D) {
+        return points.remove(point3D);
+    }
 }
