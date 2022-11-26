8547388629898e604420144f8d40f95184c05784
Eugene Elantsev <eugene_el@mail.ru>
2022-11-26 23:26:38 +0700

    create Point3D fields n methods

--- src/main/java/ru/bcomms/modelElements/Point3D.java
+++ src/main/java/ru/bcomms/modelElements/Point3D.java
@@ -1,7 +1,40 @@
 package ru.bcomms.modelElements;
 
 public class Point3D {
-    /*
-    Some fields, methods here
-     */
+    private double x, y, z;
+
+    public Point3D() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+    public Point3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
 }
