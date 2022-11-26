dbd1c7e51b2a7952150d91f4e1ed169e60aa056d
Eugene Elantsev <eugene_el@mail.ru>
2022-11-27 00:20:06 +0700

    Camera class done

--- src/main/java/ru/bcomms/modelElements/Camera.java
+++ src/main/java/ru/bcomms/modelElements/Camera.java
@@ -1,4 +1,33 @@
 package ru.bcomms.modelElements;
 
 public class Camera {
+    private Point3D location;
+    private Angle3D angle;
+
+    public Camera(Point3D location, Angle3D angle) {
+        this.location = location;
+        this.angle = angle;
+    }
+
+    public void rotate(Angle3D angle) {
+        this.angle.setAngleXAxe(this.angle.getAngleXAxe() + angle.getAngleXAxe());
+        this.angle.setAngleYAxe(this.angle.getAngleYAxe() + angle.getAngleYAxe());
+        this.angle.setAngleZAxe(this.angle.getAngleZAxe() + angle.getAngleZAxe());
+    }
+
+    public void move(Point3D point) {
+        this.setLocation(point);
+    }
+
+    private void setLocation(Point3D location) {
+        this.location = location;
+    }
+
+    public Point3D getLocation() {
+        return location;
+    }
+
+    public Angle3D getAngle() {
+        return angle;
+    }
 }
