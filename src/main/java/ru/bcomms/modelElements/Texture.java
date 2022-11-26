89fdcc90c4aa8546c704a70e8b8a1a6ed01a19ab
Eugene Elantsev <eugene_el@mail.ru>
2022-11-26 23:49:56 +0700

    create Texture class

--- src/main/java/ru/bcomms/modelElements/Texture.java
+++ src/main/java/ru/bcomms/modelElements/Texture.java
@@ -1,7 +1,28 @@
 package ru.bcomms.modelElements;
 
+import java.awt.*;
+import java.util.UUID;
+
 public class Texture {
-    /*
-    Some fields, methods here
-     */
+    private final UUID uuid;
+    private final String name;
+    private final Image image;
+
+    public Texture(String name, Image image) {
+        this.uuid = UUID.randomUUID();
+        this.name = name;
+        this.image = image;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Image getImage() {
+        return image;
+    }
 }
